d8560e81ba28ceff9015567383c22114f3b5f080
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-27 14:13:49 +0545

    wXo2m0FiyK1gmEWA

--- backend/Controller/ProductController.js
+++ backend/Controller/ProductController.js
@@ -1,68 +1,81 @@
 const Product = require('../model/Product')
 
-const product1 = new Product({
-  productName: "Apple",
-  category: "fruit",
-  price: 40,
-  Quantity: 1000,
-})
+// const product1 = new Product({
+//   productName: "Apple",
+//   category: "fruit",
+//   price: 40,
+//   Quantity: 1000,
+// })
 
-product1.save()
-  .then((res) => {
-    console.log("saved successfully", res)
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-Product.insertMany([
-  {
-    productName: "Banana",
-    category: "fruit",
-    price: 20,
-    Quantity: 500,
-  },
-  {
-    productName: "Carrot",
-    category: "vegetable",
-    price: 15,
-    Quantity: 800,
-  },
-  {
-    productName: "Orange",
-    category: "fruit",
-    price: 50,
-    Quantity: 600,
-  },
-  {
-    productName: "Tomato",
-    category: "vegetable",
-    price: 25,
-    Quantity: 400,
-  },
-  {
-    productName: "Milk",
-    category: "dairy",
-    price: 60,
-    Quantity: 300,
-  },
-  {
-    productName: "Bread",
-    category: "bakery",
-    price: 35,
-    Quantity: 200,
-  },
-  {
-    productName: "Cheese",
-    category: "dairy",
-    price: 150,
-    Quantity: 100,
-  },
-  {
-    productName: "Potato",
-    category: "vegetable",
-    price: 10,
-    Quantity: 1200,
-  },
-])
-  .then((res) => { console.log("successfully inserted.", res) })
-  .catch((err) => { console.error("isn't inserted", err) })
+// Product.insertMany([
+//   {
+//     productName: "Banana",
+//     category: "fruit",
+//     price: 20,
+//     Quantity: 500,
+//   },
+//   {
+//     productName: "Carrot",
+//     category: "vegetable",
+//     price: 15,
+//     Quantity: 800,
+//   },
+//   {
+//     productName: "Orange",
+//     category: "fruit",
+//     price: 50,
+//     Quantity: 600,
+//   },
+//   {
+//     productName: "Tomato",
+//     category: "vegetable",
+//     price: 25,
+//     Quantity: 400,
+//   },
+//   {
+//     productName: "Milk",
+//     category: "dairy",
+//     price: 60,
+//     Quantity: 300,
+//   },
+//   {
+//     productName: "Bread",
+//     category: "bakery",
+//     price: 35,
+//     Quantity: 200,
+//   },
+//   {
+//     productName: "Cheese",
+//     category: "dairy",
+//     price: 150,
+//     Quantity: 100,
+//   },
+//   {
+//     productName: "Potato",
+//     category: "vegetable",
+//     price: 10,
+//     Quantity: 1200,
+//   },
+// ])
+//   .then((res) => { console.log("successfully inserted.", res) })
+//   .catch((err) => { console.error("isn't inserted", err) })
+
+async function addProduct(req, res) {
+  try {
+    // const {productName}= req.body;
+    const product1 = new Product({
+      productName: "Apple",
+      category: "fruit",
+      price: 40,
+      Quantity: 1000,
+    })
+    product1.save();
+    console.log("product successfully added.");
+    res.status(201).send(product1);
+  } catch (err) {
+    console.log("not inserted.")
+  }
+
+}
+
+module.exports = { addProduct }
