36e1974ff87440c2b193ffb297812767910efb6d
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-28 17:42:00 +0545

    index backend file

---
+++ frontend/src/Pages/Client/index.jsx
@@ -0,0 +1,17 @@
+import Header from "../../Components/Header"
+import SideBar from "../../Components/SideBar"
+
+
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <SideBar />
+    </>
+
+  )
+}
+
+export default Index
