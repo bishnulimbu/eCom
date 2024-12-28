bdacd053e8a4a840366eefa48f7cecf763e687d2
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-28 17:41:49 +0545

    making clean code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import axios from 'axios'
+import Index from './Pages/Client/index'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const getWeatherData = () => {
-    axios.get("http://api.weatherapi.com/v1/current.json?key=88e528f1ad62402897a80054242512&q=Kathmandu&aqi=no")
-      .then(function (response) {
-        console.log(response)
-      })
-  }
-  // 88e528f1ad62402897a80054242512
 
   return (
     <>
-      <h1>hello! This is the index page.</h1>
-      <p>{getWeatherData()}</p>
+      <Index />
 
 
 
