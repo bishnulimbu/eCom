2ee302666a0a924c1ef36e287317d04104695b5c
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-28 17:43:07 +0545

    updated route

--- backend/Routes/productRoute.js
+++ backend/Routes/productRoute.js
@@ -2,9 +2,11 @@ const express = require('express')
 // const app = express();
 
 const { addProduct } = require('../Controller/ProductController')
+const { upload } = require('../Middleware/upload');
 const router = express.Router();
 
 router.post("/addproduct", addProduct);
+router.post("/addimage", upload);
 
 module.exports = router;
 
