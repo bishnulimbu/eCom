ac70705bf8cc6d709cca8ece5b22fed6a4f80b78
Bishnu Limbu <subashlimbu987654321@gmail.com>
2025-01-03 20:40:26 +0545

    main jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
-import Index from './Pages/Client/index'
+import AdminIndex from './Pages/Admin/AdminIndex'
+import { Routes, Route } from 'react-router-dom';
+import Product from './Pages/Admin/Product';
+import DashBoard from './Pages/Admin/DashBoard';
 
 function App() {
 
   return (
     <>
-      <Index />
-
-
-
+      <Routes>
+        <Route path='/' element={<AdminIndex />} >
+          <Route path='/product' element={<Product />} />
+          <Route path='/dashboard' element={<DashBoard />} />
+        </Route>
+      </Routes>
     </>
   )
 }
