9aa129b13f9fa95b9387ae9ead77f768c878f44f
Bishnu Limbu <subashlimbu987654321@gmail.com>
2025-01-06 16:48:04 +0545

    sidebar

--- frontend/src/Components/SideBar.jsx
+++ frontend/src/Components/SideBar.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
 import './SideBar.css'
 
 const SideBar = () => {
   const links = [
-    { lName: "Dashboard", address: "/DashBoard" },
+    { lName: "Dashboard", address: "/dashboard" },
     { lName: "View Products", address: "/product" },
+    { lName: "Add Product", address: "/addProduct" },
     { lName: "User Details", address: "/user" },
   ];
   return (
     <div className="link-list">
       <ul>
         {links.map((val, index) => (
-          <li key={index}><Link to={val.address}>{val.lName}</Link>
+          <li key={index}>
+            <Link to={val.address}>{val.lName}</Link>
           </li>
         ))}
       </ul>
