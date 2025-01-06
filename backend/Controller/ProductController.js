cf05583d38909aae996ac02a2add8d137bebbf2c
Bishnu Limbu <subashlimbu987654321@gmail.com>
2025-01-06 16:46:48 +0545

    product controller updated for view and add

--- backend/Controller/ProductController.js
+++ backend/Controller/ProductController.js
@@ -1,4 +1,4 @@
-const Product = require('../model/Product')
+const Product = require('../Model/Product')
 
 
 async function addProduct(req, res) {
@@ -19,63 +19,13 @@ async function addProduct(req, res) {
 
 }
 
-module.exports = { addProduct }
-//   productName: "Apple",
-//   category: "fruit",
-//   price: 40,
-//   Quantity: 1000,
-// })
+async function allProduct(req, res) {
+  try {
+    const allProducts = await Product.find();
+    res.status(200).json({ message: "success", data: allProducts });
+  } catch (e) {
+    res.status(500).json({ message: "unable to fetch data." })
+  }
+}
 
-// Product.insertMany([
-//   {
-//     productName: "Banana",
-//     category: "fruit",
-//     price: 20,
-//     Quantity: 500,
-//   },
-//   {
-//     productName: "Carrot",
-//     category: "vegetable",
-//     price: 15,
-//     Quantity: 800,
-//   },
-//   {
-//     productName: "Orange",
-//     category: "fruit",
-//     price: 50,
-// const product1 = new Product({
-//     Quantity: 600,
-//   },
-//   {
-//     productName: "Tomato",
-//     category: "vegetable",
-//     price: 25,
-//     Quantity: 400,
-//   },
-//   {
-//     productName: "Milk",
-//     category: "dairy",
-//     price: 60,
-//     Quantity: 300,
-//   },
-//   {
-//     productName: "Bread",
-//     category: "bakery",
-//     price: 35,
-//     Quantity: 200,
-//   },
-//   {
-//     productName: "Cheese",
-//     category: "dairy",
-//     price: 150,
-//     Quantity: 100,
-//   },
-//   {
-//     productName: "Potato",
-//     category: "vegetable",
-//     price: 10,
-//     Quantity: 1200,
-//   },
-// ])
-//   .then((res) => { console.log("successfully inserted.", res) })
-//   .catch((err) => { console.error("isn't inserted", err) })
+module.exports = { addProduct, allProduct }
