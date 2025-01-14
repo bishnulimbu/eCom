1b3a0182c956f38fbb10a6d459f534d43d7870f3
bishnulimbu <subashlimbu0987654321@gmail.com>
2025-01-14 16:35:24 +0545

    added the table and add functionality

--- frontend/src/Pages/Admin/Product.jsx
+++ frontend/src/Pages/Admin/Product.jsx
@@ -6,6 +6,7 @@ const Product = () => {
   const [pData, setPdata] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [result, setResult] = useState('');
 
   useEffect(() => {
     const getData = async () => {
@@ -22,6 +23,25 @@ const Product = () => {
     getData();
   }, [])
 
+  const deleteFn = async(id)=>{
+    try{
+      await axios.delete(`http://localhost:8080/product/deleteProduct/${id}`)
+      console.log("successfully deleted.");
+      setResult("successfully deleted the product.");
+      setPdata(pData.filter(product=>product._id!== id));
+    }catch(e){
+      console.error("error: ",e.message);
+    }
+  }
+  useEffect(()=>{
+    if(result){
+    const timer = setTimeout(()=>{
+        setResult('');
+    },3000)
+      return()=>clearTimeout(timer);
+    }
+  },[result])
+
   if (loading) {
     return <h1>Loading...</h1>
   }
@@ -33,12 +53,32 @@ const Product = () => {
   return (
     <>
       <div className="mainCard">
-        {pData.length > 0 ? pData.map((product) => (<div className='productCard' key={product._id} >
-          <p>Name: {product.productName}</p>
-          <p>Category: {product.category}</p>
-          <p>Price: Nrs.{product.price}</p>
-          <p>Quantity: {product.Quantity}</p>
-        </div>)) : (<h1>no product to displayed.</h1>)}
+        <table border="1" style={{borderCollapse: 'collapse'}}>
+          <thead>
+            <tr>
+              <th>Product names</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Quanity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pData.length > 0 ? pData.map((product) => (<tr key={product._id}>
+              <td>{product.productName}</td>
+              <td>{product.category}</td>
+              <td>Nrs.{product.price}</td>
+              <td>{product.quantity}</td>
+              <td className="btn1">
+                  <button type="">Edit</button>
+                  <button type="button" onClick={()=>deleteFn(product._id)}>Delete</button>
+              </td>
+            </tr>
+            )) : (<h1>no product to displayed</h1>)}
+          </tbody>
+        </table>
+        {result && <p>Message:  {result}</p>}
+
       </div>
     </>
 
