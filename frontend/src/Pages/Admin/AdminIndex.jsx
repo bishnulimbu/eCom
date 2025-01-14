91f99144642711b8a3bb5eff550d3996f86de3ae
bishnulimbu <subashlimbu0987654321@gmail.com>
2025-01-14 16:34:55 +0545

    routes defination

--- frontend/src/Pages/Admin/AdminIndex.jsx
+++ frontend/src/Pages/Admin/AdminIndex.jsx
@@ -7,18 +7,22 @@ import './AdminIndex.css'
 import AddProduct from "./AddProduct";
 
 const AdminIndex = () => {
+
+
   return (
     <>
-      <div className="admin-header"><Header />
-        <div className="admin-sidebar"><SideBar />
-          <div className="admin-content">          <Routes>
-            <Route path='/' element={<DashBoard />} />
-            <Route path='/dashboard' element={<DashBoard />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/addProduct" element={<AddProduct />} />
-            <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-          </Routes>
-          </div>
+<div className="hover-area"></div>
+<div className="admin-header">
+  <Header />
+</div>
+      <div className="admin-sidebar"><SideBar />
+        <div className="admin-content">          <Routes>
+          <Route path='/' element={<DashBoard />} />
+          <Route path='/dashboard' element={<DashBoard />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/addProduct" element={<AddProduct />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
         </div>
 
       </div>
