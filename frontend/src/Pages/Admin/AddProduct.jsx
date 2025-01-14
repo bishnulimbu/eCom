5c162bdcd9136d1849f320433cfa6e5212dbea6b
bishnulimbu <subashlimbu0987654321@gmail.com>
2025-01-14 16:34:30 +0545

    add product finally done

--- frontend/src/Pages/Admin/AddProduct.jsx
+++ frontend/src/Pages/Admin/AddProduct.jsx
@@ -1,25 +1,73 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import './AddProduct.css'
+import axios from 'axios';
 
 const AddProduct = () => {
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('');
+  const [price, setPrice] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [result, setResult] = useState(false);
+  const [error,setError] = useState('');
+
+
+  const addFn= async(e) =>{
+    e.preventDefault();
+    try{
+      console.log("data sent",{
+       productName:name,
+        category:category,
+        price:price,
+        quantity:quantity
+      });
+      const response = await axios.post('http://localhost:8080/product/addProduct',{
+       productName:name,
+        category:category,
+        price:price,
+        quantity:quantity
+      });
+       console.log(response);
+      setResult(true);
+      setName('');
+      setCategory('');
+      setPrice('');
+      setQuantity('');
+    }catch(e){
+      setError(e.message);
+      console.error("couldn't add data.",e);
+    }
+  }
+  useEffect(()=>{
+    if(result || error){
+      const timer = setTimeout(()=>{
+        setError('');
+        setResult(false);
+      },3000)
+      return()=>clearTimeout(timer);
+    }
+  },[result,error])
+
   return (
-    <form className="mainForm" action="">
+    <form className="mainForm" action="" onSubmit={addFn}>
       <label htmlFor="name">Name:</label>
-      <input type="text" id="name" required />
+      <input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)} required />
 
       <label htmlFor="category">Category:</label>
-      <input type="text" id="category" required />
+      <input type="text" id="category" value={category} onChange={(e)=>setCategory(e.target.value)} required />
 
       <label htmlFor="price">Price:</label>
-      <input type="number" id="price" required />
+      <input type="number" id="price" value={price} onChange={(e)=>setPrice(e.target.value)} required />
 
       <label htmlFor="quantity">Quantity:</label>
-      <input type="number" id="quantity" required />
+      <input type="number" id="quantity" value={quantity} onChange={(e)=>setQuantity(e.target.value)} required />
 
-      <button type="submit">Submit</button>
+      <button type="submit" >Submit</button>
+      {result && <p>data added successfully.</p>}
+      {error && <p>{error}</p>}
     </form>
 
   )
 }
+    
 
-export default AddProduct
+    export default AddProduct
