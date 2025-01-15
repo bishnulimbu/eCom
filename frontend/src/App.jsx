d1e108e5c705ae83e1970c3220612999f43747b0
bishnulimbu <subashlimbu0987654321@gmail.com>
2025-01-15 12:50:28 +0545

    removed the route bad practise

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
 import AdminIndex from './Pages/Admin/AdminIndex'
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <Routes>
-
-        <Route path='/*' element={<AdminIndex />} />
-        
-      </Routes>
+      <AdminIndex />
     </>
   )
 }
