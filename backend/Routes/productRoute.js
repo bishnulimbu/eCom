eec92f8f6a6aa7d7327c483637d232d9cf9fd140
bishnulimbu <subashlimbu0987654321@gmail.com>
2025-01-15 12:48:33 +0545

    added routes for getting specific products and updating

--- backend/Routes/productRoute.js
+++ backend/Routes/productRoute.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
-const { addProduct, allProduct, deleteProduct} = require('../Controller/ProductController')
+const { addProduct,getProduct, allProduct, deleteProduct, editProduct} = require('../Controller/ProductController')
 // const { upload } = require('../Middleware/upload');
 const router = express.Router();
 
 router.post("/addProduct", addProduct);
 router.get("/allProduct", allProduct);
+router.get('/getProduct/:id',getProduct)
 router.delete("/deleteProduct/:id",deleteProduct);
+router.patch("/editProduct/:id",editProduct)
 // router.post("/addimage", upload);
 
 module.exports = router;
